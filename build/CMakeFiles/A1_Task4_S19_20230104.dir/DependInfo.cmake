80f7694d73b3abfb9ad8bac7d39c5ec2ade74e47
Ahmed Atef <ahmedatef@Ahmeds-MacBook-Air.local>
2024-11-05 23:20:23 +0200

    added alu execute function still program not working

---
+++ build/CMakeFiles/A1_Task4_S19_20230104.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ahmedatef/CLionProjects/vole-machine/Alu.cpp" "CMakeFiles/A1_Task4_S19_20230104.dir/Alu.cpp.o" "gcc" "CMakeFiles/A1_Task4_S19_20230104.dir/Alu.cpp.o.d"
+  "/Users/ahmedatef/CLionProjects/vole-machine/ControlUnit.cpp" "CMakeFiles/A1_Task4_S19_20230104.dir/ControlUnit.cpp.o" "gcc" "CMakeFiles/A1_Task4_S19_20230104.dir/ControlUnit.cpp.o.d"
+  "/Users/ahmedatef/CLionProjects/vole-machine/cpu.cpp" "CMakeFiles/A1_Task4_S19_20230104.dir/cpu.cpp.o" "gcc" "CMakeFiles/A1_Task4_S19_20230104.dir/cpu.cpp.o.d"
+  "/Users/ahmedatef/CLionProjects/vole-machine/machine.cpp" "CMakeFiles/A1_Task4_S19_20230104.dir/machine.cpp.o" "gcc" "CMakeFiles/A1_Task4_S19_20230104.dir/machine.cpp.o.d"
+  "/Users/ahmedatef/CLionProjects/vole-machine/main.cpp" "CMakeFiles/A1_Task4_S19_20230104.dir/main.cpp.o" "gcc" "CMakeFiles/A1_Task4_S19_20230104.dir/main.cpp.o.d"
+  "/Users/ahmedatef/CLionProjects/vole-machine/mainUi.cpp" "CMakeFiles/A1_Task4_S19_20230104.dir/mainUi.cpp.o" "gcc" "CMakeFiles/A1_Task4_S19_20230104.dir/mainUi.cpp.o.d"
+  "/Users/ahmedatef/CLionProjects/vole-machine/memory.cpp" "CMakeFiles/A1_Task4_S19_20230104.dir/memory.cpp.o" "gcc" "CMakeFiles/A1_Task4_S19_20230104.dir/memory.cpp.o.d"
+  "/Users/ahmedatef/CLionProjects/vole-machine/register.cpp" "CMakeFiles/A1_Task4_S19_20230104.dir/register.cpp.o" "gcc" "CMakeFiles/A1_Task4_S19_20230104.dir/register.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/A1_Task4_S19_20230104.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "A1_Task4_S19_20230104"
+  "A1_Task4_S19_20230104.pdb"
+  "CMakeFiles/A1_Task4_S19_20230104.dir/Alu.cpp.o"
+  "CMakeFiles/A1_Task4_S19_20230104.dir/Alu.cpp.o.d"
+  "CMakeFiles/A1_Task4_S19_20230104.dir/ControlUnit.cpp.o"
+  "CMakeFiles/A1_Task4_S19_20230104.dir/ControlUnit.cpp.o.d"
+  "CMakeFiles/A1_Task4_S19_20230104.dir/cpu.cpp.o"
+  "CMakeFiles/A1_Task4_S19_20230104.dir/cpu.cpp.o.d"
+  "CMakeFiles/A1_Task4_S19_20230104.dir/machine.cpp.o"
+  "CMakeFiles/A1_Task4_S19_20230104.dir/machine.cpp.o.d"
+  "CMakeFiles/A1_Task4_S19_20230104.dir/main.cpp.o"
+  "CMakeFiles/A1_Task4_S19_20230104.dir/main.cpp.o.d"
+  "CMakeFiles/A1_Task4_S19_20230104.dir/mainUi.cpp.o"
+  "CMakeFiles/A1_Task4_S19_20230104.dir/mainUi.cpp.o.d"
+  "CMakeFiles/A1_Task4_S19_20230104.dir/memory.cpp.o"
+  "CMakeFiles/A1_Task4_S19_20230104.dir/memory.cpp.o.d"
+  "CMakeFiles/A1_Task4_S19_20230104.dir/register.cpp.o"
+  "CMakeFiles/A1_Task4_S19_20230104.dir/register.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/A1_Task4_S19_20230104.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
